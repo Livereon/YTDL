3fc7b535f4335a009a884961a119854bf2753590
Livereon <wahabalimhaid123@gmail.com>
2022-03-05 21:09:38 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -4,45 +4,64 @@ from kivy.uix.boxlayout import BoxLayout
 import os, sys
 from kivy.resources import resource_add_path, resource_find
 from yt_dlp import YoutubeDL
+import pyperclip
 
-try:
-    class UI(BoxLayout):
+def format_selector(ctx):
 
-        def  ydl_opt(self, PATH):
-            ydl_opts = {
-                'format': 'best',
-                'paths': {'home': PATH}
+    pass
+
+validURL = {
+    "https://www.youtube.com",
+    "https://www.tiktok.com",
+    "https://www.crunchyroll.com"
+}
+
+class UI(BoxLayout):
+
+    def ydl_opt(self, PATH):
+        ydl_opts = {
+            'format': 'best',
+            'paths': {'home': PATH},
+            'format_sort': {
+                'res': f"{self.ids.resspinner.text[:-1]}",
+                'ext': "mp4"
+
             }
-            return ydl_opts
+        }
+        return ydl_opts
 
-        def process(self):
-            LINK = self.ids.link.text
-            return LINK
-
-        def down_button(self):
-            with YoutubeDL(self.ydl_opt(self.ids.path.text)) as ydl:
-                if self.process() == "":
-                    warnings.warn("There is no link BAKA!")
-                    return
-                elif str(self.process()).startswith("https://www.youtube.com"):
-                    ydl.download([self.process()])
-                else:
-                    warnings.warn("Ara ara! Use a youtube link!")
-                    return
-            pass
-
-
-    class YTDLApp(App):
+    def pasteButton(self):
+        pastedlink = pyperclip.paste()
+        self.ids.link.text = pastedlink
         pass
 
+    def process(self):
+        links = self.ids.link.text
+        return links
 
-    if __name__ == '__main__':
-        if hasattr(sys, '_MEIPASS'):
-            resource_add_path(os.path.join(sys._MEIPASS))
-        YTDLApp().run()
+    def down_button(self):
+        with YoutubeDL(self.ydl_opt(self.ids.path.text)) as ydl:
+            link = str(self.process())
+            if any(link.startswith(x) for x in validURL):
+                ydl.download([self.process()])
+                return
+            elif link == "":
+                print("There is no link BAKA!")
+                return
+            else:
+                print("Ara ara! Use a Youtube,TikTok or Crunchyroll link!")
+                return
+        pass
 
-except warnings.catch_warnings as e:
-    print("Uh Oh!", e)
-pass
+##"
+class YTDLApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    if hasattr(sys, '_MEIPASS'):
+        resource_add_path(os.path.join(sys._MEIPASS))
+    YTDLApp().run()
+
 
 
